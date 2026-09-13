52f6882d4d12f2a13a26f3480e63382101fd8c72
Marcos Scaldelai <marcos.scaldelai.code@gmail.com>
2026-09-13 01:09:51 -0300

    refactor: simplifica lógica e fecha Scanner

--- src/ClassificadorDeTemperatura.java
+++ src/ClassificadorDeTemperatura.java
@@ -1,25 +1,23 @@
-import java.util.Scanner;
+void main() {
+    Scanner scanner = new Scanner(System.in);
 
-public class ClassificadorDeTemperatura {
-    static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    final double temperatura;
+    final String statusTemperatura;
 
-        final double temperatura;
-        final String statusTemperatura;
+    IO.print("> Digite a temperatura: ");
+    temperatura = scanner.nextDouble();
 
-        System.out.print("> Digite a temperatura: ");
-        temperatura = scanner.nextDouble();
-
-        if (temperatura < 10) {
-            statusTemperatura = "muito frio";
-        } else if (temperatura >= 10 && temperatura < 20) {
-            statusTemperatura = "frio";
-        } else if (temperatura >= 20 && temperatura < 30) {
-            statusTemperatura = "agradável";
-        } else {
-            statusTemperatura = "quente";
-        }
-
-        System.out.printf("O clima hoje está: %s.", statusTemperatura);
+    if (temperatura < 10) {
+        statusTemperatura = "muito frio";
+    } else if (temperatura < 20) {
+        statusTemperatura = "frio";
+    } else if (temperatura < 30) {
+        statusTemperatura = "agradável";
+    } else {
+        statusTemperatura = "quente";
     }
+
+    System.out.printf("O clima hoje está: %s.", statusTemperatura);
+
+    scanner.close();
 }
