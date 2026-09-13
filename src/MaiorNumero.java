612a9d9fd4c2aab99eb432341bd54b493c858440
Marcos Scaldelai <marcos.scaldelai.code@gmail.com>
2026-09-13 01:56:05 -0300

    refactor: converte para arquivo compacto e ajusta formatação

--- src/MaiorNumero.java
+++ src/MaiorNumero.java
@@ -1,36 +1,34 @@
-import java.util.Scanner;
+void main() {
+    Scanner scanner = new Scanner(System.in);
 
-public class MaiorNumero {
-    static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    int primeiroNumero;
+    int segundoNumero;
+    int terceiroNumero;
 
-        int primeiroNumero;
-        int segundoNumero;
-        int terceiroNumero;
+    int maiorNumero;
 
-        int maiorNumero;
+    IO.print("> Primeiro número: ");
+    primeiroNumero = scanner.nextInt();
 
-        System.out.print("> Primeiro número: ");
-        primeiroNumero = scanner.nextInt();
+    IO.print("> Segundo número: ");
+    segundoNumero = scanner.nextInt();
 
-        System.out.print("> Segundo número: ");
-        segundoNumero = scanner.nextInt();
+    IO.print("> Terceiro número: ");
+    terceiroNumero = scanner.nextInt();
 
-        System.out.print("> Terceiro número: ");
-        terceiroNumero = scanner.nextInt();
+    maiorNumero = primeiroNumero;
 
-        maiorNumero = primeiroNumero;
-
-        if (segundoNumero > maiorNumero) {
-            maiorNumero = segundoNumero;
-        } if (terceiroNumero > maiorNumero) {
-            maiorNumero = terceiroNumero;
-        } if (primeiroNumero == segundoNumero && segundoNumero == terceiroNumero) {
-            System.out.println("Os três números são iguais.");
-        } else {
-            System.out.printf("O maior número é: %s%n", maiorNumero);
-        }
-
-        scanner.close();
+    if (segundoNumero > maiorNumero) {
+        maiorNumero = segundoNumero;
     }
+    if (terceiroNumero > maiorNumero) {
+        maiorNumero = terceiroNumero;
+    }
+    if (primeiroNumero == segundoNumero && segundoNumero == terceiroNumero) {
+        IO.println("Os três números são iguais.");
+    } else {
+        System.out.printf("O maior número é: %d%n", maiorNumero);
+    }
+
+    scanner.close();
 }
